f06271af993495461189f044e85179bb744b304d
elonehoo <hi@elonehoo.me>
2024-08-27 14:08:10 +0800

    ci: build to zeabur

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,11 +26,4 @@ export default defineNuxtConfig({
       enabled: true,
     },
   },
-  vite: {
-    resolve: {
-      alias: {
-        '.prisma/client/index-browser': './node_modules/.prisma/client/index-browser.js',
-      },
-    },
-  },
 })
