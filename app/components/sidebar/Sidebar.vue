5b50757d9b1ce9e6f7aa39e3af193f9e4d920281
CaileanSong <114227985+CaileanSong@users.noreply.github.com>
2024-10-04 14:53:01 +0800

    feat: add sidebar

--- app/components/sidebar/Sidebar.vue
+++ app/components/sidebar/Sidebar.vue
@@ -3,9 +3,9 @@
 </script>
 
 <template>
-  <div>
+  <div class="w-full">
     <LazySidebarIcon class="h-14 items-center flex flex-[0_0_auto] p-x-16px justify-between" />
     <LazySidebarUser class="p-x-16px" />
-    <LazySidebarSearch />
+    <LazySidebarSearch class="mt-5 w-10/12" />
   </div>
 </template>
